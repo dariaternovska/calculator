a61ef6baa984e67fe827eef764ab260d9630b81a
Daria Ternovska <119539125+dariaternovska@users.noreply.github.com>
2025-08-21 23:37:24 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter 2 number");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Operation");
+        String action = scanner.next();
+        int res;
+
+        switch (action){
+            case "+":
+                res = num1 + num2;
+                System.out.println("Result: " + res);
+                break;
+            case "*" :
+                res = num1 * num2;
+                System.out.println("Result: " + res);
+                break;
+            case "/" :
+                if (num2 == 0)
+                    System.out.println("Error");
+                else {
+                    res = num1 / num2;
+                    System.out.println("Result: " + res);
+                }
+                break;
+            default:
+                System.out.println("Unknown operation");git --version
+        }
+    }
+}
